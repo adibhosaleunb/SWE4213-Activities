d480a2227fa19fcf79f18f47fb75f99f05ffbfb9
adibhosaleunb <aditya.bhosale@unb.ca>
2026-02-15 00:37:38 -0400

    SWE4213_docker_Activity1_completed

---
+++ 1-HelloExpres/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello from Aditya Bhosale');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
